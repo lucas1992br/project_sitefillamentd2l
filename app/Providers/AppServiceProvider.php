f633cdb325b71211f00798ed72dc39a76df259af
Lucas1992br <pkk.master@gmail.com>
2026-05-06 16:52:41 -0300

    erro td

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        require_once base_path('app/helpers.php');
     }
 
     public function boot(): void
